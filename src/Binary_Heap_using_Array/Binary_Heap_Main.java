ef3d8a65a55f18410573df4af6d88ec5a48bbe2d
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-04-24 16:16:17 +0530

    After creating a menu driver for this program...

--- src/Binary_Heap_using_Array/Binary_Heap_Main.java
+++ src/Binary_Heap_using_Array/Binary_Heap_Main.java
@@ -1,5 +1,7 @@
 package Binary_Heap_using_Array;
 
+import java.util.Scanner;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Abhinav Bhardwaj
@@ -9,23 +11,34 @@ package Binary_Heap_using_Array;
 
 public class Binary_Heap_Main {
         public static void main(String[] args) {
-            Binary_Heap_Array B_H_A = new Binary_Heap_Array(5);
-            B_H_A.insert(12);
-            B_H_A.insert(7);
-            B_H_A.insert(6);
-            B_H_A.insert(18);
-            B_H_A.insert(8);
-            B_H_A.display_array();
-            System.out.println(B_H_A.SizeofTree());
+            Scanner obj = new Scanner(System.in);
             System.out.println();
-            System.out.println(B_H_A.extractValue());
-            System.out.println(B_H_A.extractValue());
-            System.out.println(B_H_A.extractValue());
-            System.out.println(B_H_A.extractValue());
-            System.out.println(B_H_A.extractValue());
+            System.out.print("Enter the no. of elements you want to insert in tree :  ");
+            int size = obj.nextInt();
             System.out.println();
-            System.out.println(B_H_A.SizeofTree());
+            Binary_Heap_Array B_H_A = new Binary_Heap_Array(size);
+            int element , option ;
+            for ( int i = 0 ; i < size ; i++ ){
+                if(i != 0) System.out.print("Enter the next element you want to insert in the tree   :  ");
+                else System.out.print("Enter the first element in the tree  :  ");
+                element = obj.nextInt();
+                B_H_A.insert(element);
+            }
             System.out.println();
-            B_H_A.display_array();
+            boolean Flag = true;
+            while (Flag) {
+                System.out.println("Press 1 : To display the elements of the tree.");
+                System.out.println("Press 2 : To extract value from the tree.");
+                System.out.println("Press 3 : To find the no. of elements in the tree.");
+                System.out.println("Press 4 : To exit.");
+                System.out.println();
+                System.out.print("Enter your choice :  ");
+                option = obj.nextInt();
+                if ( option == 1 ) B_H_A.display_array();
+                else if ( option == 2 ) B_H_A.extractValue();
+                else if ( option == 3 ) System.out.println("Currently there are " + B_H_A.sizeofTree + "elements in the tree.");
+                else if ( option == 4 ) Flag = false;
+                else System.out.println(" Warning !!! Please select a valid option... ");
+            }
         }
     }
